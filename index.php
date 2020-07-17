4b3401bf44a54221ef71e58194d11f9724739381
sumi yoshihiro <ak.sumi.yoshihiro@gmail.com>
2020-07-17 15:39:26 +0900

    最初のコミット

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html><html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,26 +7,21 @@
 </head>
 <body>
     <h1>お問い合わせ情報入力</h1>
-    <form action="check.php"
-    method="post">
-   <div>
-   ニックネーム<br>
-        <input type ="text"
-        style="width: 100px;"
-        name="nickname">
-   </div>
-   <div>
-   メールアドレス<br>
-   <input type ="email"
-        style="width: 200px;"
-        name="email">
-   </div>
-   <div>
-   お問い合わせ内容<br>
-    <textarea name="content"  cols="40" rows="5">
-    </textarea>
-   </div>
-   <input type="submit" value="送信">
-   </form>
+    <form action="check.php" method="post">
+        <!-- div*3 -->
+        <div>
+            ニックネーム<br>
+            <input type="text" style="width: 100px;" name="nickname">
+        </div>
+        <div>
+            メールアドレス<br>
+            <input type="email" style="width: 200px;" name="email">
+        </div>
+        <div>
+            お問い合わせ内容<br>
+            <textarea name="content" cols="40" rows="5"></textarea>
+        </div>
+        <input type="submit" value="送信">
+    </form>
 </body>
 </html>
